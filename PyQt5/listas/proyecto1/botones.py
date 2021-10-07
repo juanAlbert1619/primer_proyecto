6c0f25b3c766b6e9716eb519b57a2f5e684473ab
juanAlbert1619 <89416569+juanAlbert1619@users.noreply.github.com>
2021-10-06 21:03:58 -0300

    subida de archivos

---
+++ PyQt5/listas/proyecto1/botones.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+
+
+class MiVentana(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("proyecto1/botones.ui", self)
+        self.boton1.clicked.connect(self.on_boton1_clicked)
+        self.boton2.clicked.connect(self.on_boton2_clicked)
+
+
+    def on_boton1_clicked(self):
+        self.boton1.setEnabled(False)
+        self.boton2.setEnabled(True)
+
+    def on_boton2_clicked(self):
+        self.boton2.setEnabled(False)
+        self.boton1.setEnabled(True)
+
+app = QApplication([])
+win = MiVentana()
+win.show()
+
+app.exec_()
+
+        
