d3818156f7552493ebde88f73f60c0a3e9fd31e5
Juan Reynoso <reynoso.juan904@gmail.com>
2021-10-15 14:45:50 -0300

    si

--- PyQt5/listas/proyecto1/lista_3a.py
+++ PyQt5/listas/proyecto1/lista_3a.py
@@ -5,23 +5,17 @@ from PyQt5 import uic
 class MiVentana(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("proyecto1/lista3a.ui", self)
+        uic.loadUi("lista3a.ui", self)
         self.agregar.clicked.connect(self.on_agregar)
-        self.eliminar.clicked.connect(self.on_agregar)
-        #self.btn_izq_clicked.connect(self.on_btn_izq)
-        
-
-        
-       
-
+      
     def on_agregar(self):
         self.lista1.addItem(self.nombre.text())
         self.nombre.setText('')
 
-    def on_eliminar(self):
-       self.lista1.setText(self.lista.currentItem().text())
-       self.lista2.addItem(self.lista1.currentItem().Text())
-       self.lista1.setText('')
+    # def on_eliminar(self):
+    #    self.lista1.setText(self.lista.currentItem().text())
+    #    self.lista2.addItem(self.lista1.currentItem().Text())
+    #    self.lista1.setText('')
 
 app = QApplication([])
 win = MiVentana()
