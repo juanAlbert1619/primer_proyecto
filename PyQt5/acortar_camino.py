9835dae793ecf644629aa1ddc3bdc875da4c5145
Juan Reynoso <reynoso.juan904@gmail.com>
2021-10-22 19:11:08 -0300

    ai

---
+++ PyQt5/acortar_camino.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+
+
+class MiVentana(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("nombre del archivo.ui", self)
+        self.combo.currentIndexChanged.connect(self.on_cambio)
+
+    def on_cambio(self):
+      
+
+app = QApplication([])
+win = MiVentana()
+win.show()
+
+app.exec_()
+
+
+
---
+++ PyQt5/comboBox/combo_box.py
@@ -0,0 +1,23 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+
+
+class MiVentana(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("comboBox/01_combo_box.ui", self)
+        self.combo.currentIndexChanged.connect(self.on_cambio)
+
+    def on_cambio(self):
+        indece = self.combo.currentIndex()
+        self.etiqueta.setText(str(indece) + ' - ' + self.combo.currentText())
+    
+
+app = QApplication([])
+win = MiVentana()
+win.show()
+
+app.exec_()
+
+
+
