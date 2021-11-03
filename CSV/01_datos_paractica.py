3f05fb1903250f7775bc5b6a27227c5ffac65fa3
Juan Reynoso <reynoso.juan904@gmail.com>
2021-11-03 20:39:17 -0300

    3-11-21nuevo

--- CSV/01_datos_paractica.py
+++ CSV/01_datos_paractica.py
@@ -18,39 +18,9 @@ import csv
 #     for row in reader:
 #         print(row)
 
-
 resultados = []
 with open('00-datos.csv') as File:
     reader = csv.DictReader(File)
     for row in reader:
         resultados.append(row)
     print (resultados)
-
-# Veamos ahora cómo escribir datos a un archivo CSV usando la función csv.writer y la clase csv.Dictwriter discutida al inicio de este tutorial.
-# myData = [["first_name", "second_name", "Grade"],
-#           ['Alex', 'Brian', 'A'],
-#           ['Tom', 'Smith', 'B']]
-
-# myFile = open('example2.csv', 'w')
-# with myFile:
-#     writer = csv.writer(myFile)
-#     writer.writerows(myData)
-    
-# print("Writing complete")
-
-
-# Escribiendo a un Archivo CSV Usando DictWriter
-#Escribamos los siguientes datos a un CSV.
-
-with open('example4.csv', 'w') as csvfile:
-    fieldnames = ['first_name', 'last_name', 'Grade']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-    writer.writeheader()
-    writer.writerow({'Grade': 'B', 'first_name': 'Alex', 'last_name': 'Brian'})
-    writer.writerow({'Grade': 'A', 'first_name': 'Rachael',
-                     'last_name': 'Rodriguez'})
-    writer.writerow({'Grade': 'B', 'first_name': 'Jane', 'last_name': 'Oscar'})
-    writer.writerow({'Grade': 'B', 'first_name': 'Jane', 'last_name': 'Loive'})
-
-print("Writing complete")
