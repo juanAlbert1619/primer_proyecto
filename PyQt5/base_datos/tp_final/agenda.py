757ce57e7435a8377a4703b3368b59f4f971a105
Juan Reynoso <reynoso.juan904@gmail.com>
2021-11-19 22:06:51 -0300

    botones guardar nuevo

--- PyQt5/base_datos/tp_final/agenda.py
+++ PyQt5/base_datos/tp_final/agenda.py
@@ -10,35 +10,30 @@ class MiVentana(QMainWindow):
         #conectar la base de datos
         self.conexion = sqlite3.connect('00-basee.db')
         self.cursor = self.conexion.cursor()
+       
+       
         self.nuevo.clicked.connect(self.on_nuevo_reg)
         self.btn_editar.clicked.connect(self.on_editar_reg)
         self.btn_guardar.clicked.connect(self.on_guardar_reg)
         self.btn_eliminar.clicked.connect(self.on_eliminar_reg)
 
-    def on_cargar(self):
-        self.cursor.execute('select * from usuarios')
-        usuarios = self.cursor.fetchall()
+    # def on_cargar(self):
+    #     self.cursor.execute('select * from usuarios')
+    #     usuarios = self.cursor.fetchall()
 
         
 
 
     def on_nuevo_reg(self): 
 
-        self.lista.addItem(self.nombre.text())
+       
         self.nombre.setText("")
-        self.lista.addItem(self.apellido.text())
         self.apellido.setText("")
-        self.lista.addItem(self.email.text())
         self.email.setText("")
-        self.lista.addItem(self.telefono.text())
         self.telefono.setText("")
-        self.lista.addItem(self.direccion.text())
         self.direccion.setText("")
-        self.lista.addItem(self.fechaNac.text())
         self.fechaNac.setText("")
-        self.lista.addItem(self.altura.text())
         self.altura.setText("")
-        self.lista.addItem(self.peso.text())
         self.peso.setText("")
 
 
@@ -46,7 +41,27 @@ class MiVentana(QMainWindow):
          pass  
 
     def on_guardar_reg(self): 
-         pass  
+        self.conexion = sqlite3.connect('00-basee.db')
+        self.cursor = self.conexion.cursor() 
+
+    #Datos
+        self.nombre = str(self.nombre.text())
+        self.apellido = str(self.apellido.text())
+        self.email = str(self.email.text())
+        self.telefono= str(self.telefono.text())
+        self.direccion= str(self.direccion.text())
+        self.fechaNac= str(self.fechaNac.text())
+        self.altura= str(self.altura.text())
+        self.peso = str(self.peso.text())
+        self.registroDatos = (self.nombre, self.apellido, self.email, self.telefono, self.direccion, self.fechaNac, self.altura, self.peso)
+
+    #Insertamos los datos en una tabla de campos
+        self.curso.execute('INSER INTO campos (nombre, apellido, email, telefono, direccion, fechaNac, altura, peso) VALUE', self.registroDatos)
+        self.conexion.commit()
+
+        self.conexion.close()
+
+        
 
     def on_eliminar_reg(self): 
          pass  
